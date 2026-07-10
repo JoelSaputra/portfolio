2995d97f615dd89193d6695f9c27bf04ff78945f
Joel Clenn Saputra <joel.clenn03@gmail.com>
2026-07-10 03:33:23 -0400

    fix

---
+++ personal-website/.next/dev/static/chunks/[project]_portfolio_personal-website_src_pages__error_0du2_q-._.js
@@ -0,0 +1,5 @@
+(globalThis["TURBOPACK_CHUNK_LISTS"] || (globalThis["TURBOPACK_CHUNK_LISTS"] = [])).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: ["static/chunks/05ot_next_dist_compiled_0vgnixv._.js","static/chunks/05ot_next_dist_shared_lib_1gkqvw4._.js","static/chunks/05ot_next_dist_client_0x1u8ba._.js","static/chunks/05ot_next_dist_1ugdbeg._.js","static/chunks/0uo7_next_dist_pages__error_0zx35a7.js","static/chunks/[next]_entry_page-loader_ts_1ya6uz3._.js","static/chunks/05ot_react-dom_1l0x9hj._.js","static/chunks/05ot_1ogqkae._.js","static/chunks/[root-of-the-server]__08j6r-g._.js"],
+    source: "entry"
+});
---
+++ personal-website/.next/dev/static/chunks/pages/_error.js
@@ -0,0 +1,13 @@
+__turbopack_load_page_chunks__("/_error", [
+  "static/chunks/05ot_next_dist_compiled_0vgnixv._.js",
+  "static/chunks/05ot_next_dist_shared_lib_1gkqvw4._.js",
+  "static/chunks/05ot_next_dist_client_0x1u8ba._.js",
+  "static/chunks/05ot_next_dist_1ugdbeg._.js",
+  "static/chunks/0uo7_next_dist_pages__error_0zx35a7.js",
+  "static/chunks/[next]_entry_page-loader_ts_1ya6uz3._.js",
+  "static/chunks/05ot_react-dom_1l0x9hj._.js",
+  "static/chunks/05ot_1ogqkae._.js",
+  "static/chunks/[root-of-the-server]__08j6r-g._.js",
+  "static/chunks/[project]_portfolio_personal-website_src_pages__error_0du2_q-._.js",
+  "static/chunks/turbopack-[project]_portfolio_personal-website_src_pages__error_1118_dt._.js"
+])
